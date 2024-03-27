604be0192e2b2714e29f06fd66f4a4ed16ec2162
edgar-505 <68522816+edgar-505@users.noreply.github.com>
2024-03-27 09:04:19 +0700

    Update master.js

--- js/master.js
+++ js/master.js
@@ -15,8 +15,8 @@ preloadImages().then(() => {
     //const effect = new RGBShiftEffect(container, itemsWrapper)
     //const effect = new SmoothFadeTransitionEffect(container, itemsWrapper)
     //const effect = new FlyEyeTransitionEffect(container, itemsWrapper)
-    //const effect = new RippleTransitionEffect(container, itemsWrapper)
-    const effect = new PerlinTransitionEffect(container, itemsWrapper)
+    const effect = new RippleTransitionEffect(container, itemsWrapper)
+    //const effect = new PerlinTransitionEffect(container, itemsWrapper)
     //const effect = new MorphTransitionEffect(container, itemsWrapper)
 
 });
